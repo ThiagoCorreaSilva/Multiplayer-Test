ef5379a57507003dad7c586f758334b103352804
MushcV2 <titi.c2006@gmail.com>
2024-08-22 18:07:40 -0300

    Animations and Flip FIXED

--- Assets/Project/Scripts/SpawnPlayers.cs
+++ Assets/Project/Scripts/SpawnPlayers.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
@@ -12,12 +11,5 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
     private void Start()
     {
         PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
-    }
-
-    public override void OnDisconnected(DisconnectCause _cause)
-    {
-        base.OnDisconnected(_cause);
-
-        Debug.LogWarningFormat("O jogador saiu devido: " + _cause);
     }
 }
