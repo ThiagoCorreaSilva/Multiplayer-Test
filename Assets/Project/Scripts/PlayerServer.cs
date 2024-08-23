cbecb05d52fc9323bab5b232c5b61a9fb82dbf83
MushcV2 <titi.c2006@gmail.com>
2024-08-22 22:10:16 -0300

    Fixed Nicknames

---
+++ Assets/Project/Scripts/PlayerServer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class PlayerServer : MonoBehaviour
+{
+    [Header("Decorations")]
+    [SerializeField] private Color[] colors;
+    [SerializeField] private string[] names;
+
+    private Transform canvas;
+    private PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+        canvas = transform.Find("Canvas");
+
+        int _rng = Random.Range(0, names.Length);
+
+        view.RPC(nameof(ActiveCanvas), RpcTarget.All, names[_rng]);
+    }
+
+    [PunRPC]
+    public void ActiveCanvas(string _name)
+    {
+        canvas.gameObject.SetActive(true);
+
+        canvas.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = _name;
+        canvas.gameObject.GetComponentInChildren<TextMeshProUGUI>().color = colors[0];
+    }
+}
--- Assets/Project/Scripts/SpawnPlayers.cs
+++ Assets/Project/Scripts/SpawnPlayers.cs
@@ -8,14 +8,9 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
     public GameObject player;
     public Transform spawnPoint;
-    [SerializeField] private Color[] colors;
-    [SerializeField] private string[] names;
 
     private void Start()
     {
         GameObject _localPlayer = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
-
-        _localPlayer.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Length)];
-        _localPlayer.GetComponentInChildren<Canvas>().GetComponentInChildren<TextMeshProUGUI>().text = names[Random.Range(0, names.Length)];
     }
 }
