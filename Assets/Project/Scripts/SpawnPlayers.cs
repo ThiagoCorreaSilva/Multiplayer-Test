600f677c22e2c791e0694233060638f5941fa0bb
MushcV2 <titi.c2006@gmail.com>
2024-08-22 21:46:20 -0300

    Random Colors and fixed bugs
    Cada jogador vai nascer com uma cor aleatoria
    Nickname encima dos jogadores (em fases de teste)

--- Assets/Project/Scripts/SpawnPlayers.cs
+++ Assets/Project/Scripts/SpawnPlayers.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
     public GameObject player;
     public Transform spawnPoint;
+    [SerializeField] private Color[] colors;
+    [SerializeField] private string[] names;
 
     private void Start()
     {
-        PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        GameObject _localPlayer = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+
+        _localPlayer.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Length)];
+        _localPlayer.GetComponentInChildren<Canvas>().GetComponentInChildren<TextMeshProUGUI>().text = names[Random.Range(0, names.Length)];
     }
 }
